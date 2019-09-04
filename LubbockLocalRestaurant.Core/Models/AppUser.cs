d2d2062adf429e6d7c1293fe7d1ca3c62705b778
sschoor163 <steven_Schoor@yahoo.com>
2019-09-04 16:50:56 -0500

    User domain Model , steven schoor, Sep 4, 2019

--- LubbockLocalRestaurant.Core/Models/AppUser.cs
+++ LubbockLocalRestaurant.Core/Models/AppUser.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
+
 
 namespace LubbockLocalRestaurant.Core.Models
 {
-    class AppUser
+    public class AppUser :IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get
+            {
+                return FirstName + " " + LastName;
+            } }
+        public IEnumerable<Review> UserReviews { get; set; }
+        public IEnumerable<Restaurant> FavoriteRestaurants { get; set; }
+        /*TODO - Add image support*/
+        public string ProfileImage { get; set; }
+        public string Address { get; set; }
     }
 }
