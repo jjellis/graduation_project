6358e2ab9f309c15968bde3a29426b22f157db59
sschoor163 <steven_Schoor@yahoo.com>
2019-09-04 17:42:51 -0500

    DbContext, steven schoor, Sep 4, 2019

---
+++ LubbockLocalRestaurantAPI.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using LubbockLocalRestaurant.Core.Models;
+
+
+
+namespace LubbockLocalRestaurantAPI.Infrastructure.Data
+{
+    public class AppDbContext: IdentityDbContext
+    {
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);  
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlite("Data Source = .. /LubbockLocalRestaurantAPI.Infrastructure/Restaurants.db");
+        }
+    }
+}
